f7de6a2cd5ae5425a70c93069affa36f605c4f64
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-01-02 19:09:38 -0300

    feat: configuring DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use App\Models\Ticket;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $employees = Employee::factory(10)->create();
+        foreach ($employees as $employee) {
+            Ticket::factory(1)->create(['employee_id' => $employee->id]);
+        }
     }
 }
