99ff6e74894d3a6a70ff9b3e9ab8e4886bacb09f
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-01-03 21:52:18 -0300

    feat: configuration deploy

---
+++ .deploy/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
+
+composer dump-autoload --no-interaction --no-dev --optimize
+
+echo "🎬 artisan commands"
+
+php artisan migrate --seed --no-interaction --force
+
+echo "🎬 start supervisord"
+
+supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
