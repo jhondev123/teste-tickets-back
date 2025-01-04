f75e5a6485c4119353dcdf97f80670a60f8e462d
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-01-03 23:15:05 -0300

    feat: configuration swagger in production

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="Tickets API",
+ *     version="1.0.0",
+ *     description="API para gerenciamento de funcionários.",
+ *     @OA\Contact(
+ *         email="jhonattan.curtarelli@outlook.com"
+ *     )
+ * )
+ */
 abstract class Controller
 {
     //
