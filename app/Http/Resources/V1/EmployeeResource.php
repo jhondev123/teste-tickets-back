fdfc8d6dd95ff251e8ff375f198e19dc0171bbd6
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-01-05 23:36:42 -0300

    fix: updated_at correctly

--- app/Http/Resources/V1/EmployeeResource.php
+++ app/Http/Resources/V1/EmployeeResource.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Utils\Cpf;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Utils\Cpf;
 
 /**
  * Class EmployeeResource
@@ -24,21 +24,21 @@ class EmployeeResource extends JsonResource
             'cpf' => Cpf::format($this->cpf),
             'situation' => $situations[$this->situation],
             'created_at' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
-            'updated_at' => Carbon::parse($this->updated_at_)->format('d/m/Y H:i:s'),
+            'updated_at' => Carbon::parse($this->updated_at)->format('d/m/Y H:i:s'),
             'tickets' => [
                 'total_quantity' => $this->tickets->count(),
                 'total_actives' => $this->tickets->where('situation', 'A')->count(),
                 'total_inactives' => $this->tickets->where('situation', 'I')->count(),
                 'tickets' => $this->tickets->map(function ($ticket) use ($situations) {
                     return [
-                            'id' => $ticket->id,
-                            'quantity' => $ticket->quantity,
-                            'situation' => $situations[$ticket->situation],
-                            'created_at' => Carbon::parse($ticket->created_at)->format('d/m/Y H:i:s'),
-                            'updated_at' => Carbon::parse($ticket->updated_at)->format('d/m/Y H:i:s')
-                        ];
-                    }),
-                ]
+                        'id' => $ticket->id,
+                        'quantity' => $ticket->quantity,
+                        'situation' => $situations[$ticket->situation],
+                        'created_at' => Carbon::parse($ticket->created_at)->format('d/m/Y H:i:s'),
+                        'updated_at' => Carbon::parse($ticket->updated_at)->format('d/m/Y H:i:s')
+                    ];
+                }),
+            ]
         ];
     }
 
